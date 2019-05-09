d57870f994f4b3979d242aa9a8c581e8f016b195
ewelinaMakowska <ewelina.makowska0@gmail.com>
2019-05-09 21:01:30 +0200

    Updates

---
+++ precache-manifest.8d803fecc1e3f02b91fbdd2761af08ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0563e975d1b0e75f0a50e7104365b2",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "31eb9b4f08006037e72a",
+    "url": "/todo_app/static/css/main.005b3a66.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/todo_app/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "31eb9b4f08006037e72a",
+    "url": "/todo_app/static/js/main.00699001.chunk.js"
+  },
+  {
+    "revision": "6c1ee6e246643c8a5a1d",
+    "url": "/todo_app/static/js/runtime~main.da743e9d.js"
+  }
+]);
